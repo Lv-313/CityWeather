8693bdbc4eda295d660ca3c2719ebe46a2686bce
Michael Naberezhnyi <naber201725@gmail.com>
2018-06-01 07:50:36 +0300

    Fabric GoogleSearchCity service is created&confirured. App is associated with this service & get response

--- controllers/mobile/frmSearchFormController/frmSearchFormController.js
+++ controllers/mobile/frmSearchFormController/frmSearchFormController.js
@@ -1,5 +1,52 @@
 define({ 
+ 
+  
+getCity:function(){
+ 
+  var integrationClient = null;
+  var cities = this.view.rchText;
+  var input = this.view.flxSearchBox.txtSearchBox;
+  
+  var serviceName = "GoogleCitySearch";
+  var operationName = "getCities";
+  var params = { "query" : input.text};
+  var headers = null;
+ 
 
- //Type your controller code here 
-
+   integrationClient = kony.sdk.getCurrentInstance().getIntegrationService("GoogleCitySearch"); 
+ 
+integrationClient.invokeOperation("getCities", headers, params , 
+	function(result) {
+  	cities.text = JSON.stringify(result.results);
+     
+	//alert("Integration Service Response is :" + JSON.stringify(result));
+	}, 
+	function(error) {
+     alert("Integration Service Failure :" + JSON.stringify(error));
+	}
+);
+  
+  
+  
+}
+  
+  /*
+  
+  login: function() {
+   var self = this;
+   let ids = kony.sdk.getCurrentInstance().getIdentityService('ITAKonyAuth');
+   this.setBrowserVisibility(true);
+   let browser = this.view.browser;
+   ids.login({
+     browserWidget: browser,
+     success_url: 'itafacebook://com.orgname.itaFacebook'
+   }, function(response) {
+     self.setBrowserVisibility(false);
+     self.view.lblFacebook.text = "Success\n\n" + JSON.stringify(response);
+     self.loginSuccess();
+   }, function(error) {
+     self.setBrowserVisibility(false);
+     alert('Failure\n\n' + JSON.stringify(error));
+   });
+ },*/
  });
--- controllers/mobile/frmSearchFormController/frmSearchFormControllerActions.js
+++ controllers/mobile/frmSearchFormController/frmSearchFormControllerActions.js
@@ -2,4 +2,9 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for btnGetCities **/
+    AS_Button_a45c38c70fe044d2aa9efcacdb87d9de: function AS_Button_a45c38c70fe044d2aa9efcacdb87d9de(eventobject) {
+        var self = this;
+        return self.getCity.call(this);
+    }
 });
