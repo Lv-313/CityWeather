36d9cd953d1f533a98dc791f28d13f7ba9c93f09
Michael Naberezhnyi <naber201725@gmail.com>
2018-06-01 08:51:07 +0300

    segment logic is done the possibility of selection and addition is realized

--- controllers/mobile/frmSearchFormController/frmSearchFormController.js
+++ controllers/mobile/frmSearchFormController/frmSearchFormController.js
@@ -1,52 +1,67 @@
 define({ 
+  
+  cityArr : undefined,
+  
+  searchCity: function() {
+    var query = this.view.flxSearchBox.txtSearchBox.text;
+    var CitySeg = this.view.flxSearch.segSearchCity;
+    
+    if (query.length >= 3) {
+      this.getCity(query);
+      this.view.flxSearch.lblMessage.isVisible = false;
+    } else{
+      CitySeg.isVisible = false;
+      this.view.flxSearch.lblMessage.isVisible = true;
+    }
+  },
  
   
-getCity:function(){
- 
-  var integrationClient = null;
-  var cities = this.view.rchText;
-  var input = this.view.flxSearchBox.txtSearchBox;
-  
-  var serviceName = "GoogleCitySearch";
-  var operationName = "getCities";
-  var params = { "query" : input.text};
-  var headers = null;
- 
+  getCity:function(query){
 
-   integrationClient = kony.sdk.getCurrentInstance().getIntegrationService("GoogleCitySearch"); 
+    var integrationClient = null;
+    var data = null;
+    var self = this;
+    var input = this.view.flxSearchBox.txtSearchBox;
+    var CitySeg = this.view.flxSearch.segSearchCity;
+    var params = { "query" : query};
+    var headers = null;
+
+
+     CitySeg.widgetDataMap = {
+      lblSearchCity:"cityName",
+      cityId:"cityId"
+
+    };
+
+     integrationClient = kony.sdk.getCurrentInstance().getIntegrationService("GoogleCitySearch"); 
+
+     integrationClient
+        .invokeOperation("getCities", headers, params, 
+                      function(result) {
+                        
+						CitySeg.isVisible = true;
+        				
+                        data =  JSON.stringify(result.results);
+                        data = JSON.parse(data);
+                        
+       					CitySeg.setData(data);
+						self.cityArr = data;
+                      }, 
+                      function(error) {
+                       	alert("Integration Service Failure :" + JSON.stringify(error));
+                      }
+  );
+    
  
-integrationClient.invokeOperation("getCities", headers, params , 
-	function(result) {
-  	cities.text = JSON.stringify(result.results);
-     
-	//alert("Integration Service Response is :" + JSON.stringify(result));
-	}, 
-	function(error) {
-     alert("Integration Service Failure :" + JSON.stringify(error));
-	}
-);
+},
   
-  
-  
+  choiseCity: function( data) { 
+    alert( this.cityArr[data].cityName+' was succesfully added to the list');
+    newCity.push(this.cityArr[data]);
+  },
+ 
+  showNewCity: function(){
+  	alert(newCity);
 }
-  
-  /*
-  
-  login: function() {
-   var self = this;
-   let ids = kony.sdk.getCurrentInstance().getIdentityService('ITAKonyAuth');
-   this.setBrowserVisibility(true);
-   let browser = this.view.browser;
-   ids.login({
-     browserWidget: browser,
-     success_url: 'itafacebook://com.orgname.itaFacebook'
-   }, function(response) {
-     self.setBrowserVisibility(false);
-     self.view.lblFacebook.text = "Success\n\n" + JSON.stringify(response);
-     self.loginSuccess();
-   }, function(error) {
-     self.setBrowserVisibility(false);
-     alert('Failure\n\n' + JSON.stringify(error));
-   });
- },*/
+ 
  });
--- controllers/mobile/frmSearchFormController/frmSearchFormControllerActions.js
+++ controllers/mobile/frmSearchFormController/frmSearchFormControllerActions.js
@@ -2,9 +2,49 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onTextChange defined for txtSearchBox **/
+    AS_TextField_g6b63931c1da4a83bb14b319c7104134: function AS_TextField_g6b63931c1da4a83bb14b319c7104134(eventobject, changedtext) {
+        var self = this;
+        return self.searchCity.call(this);
+    },
+    /** onRowClick defined for segSearchCity **/
+    AS_Segment_d22948a3ffd743dab73f9d7a03cbabcf: function AS_Segment_d22948a3ffd743dab73f9d7a03cbabcf(eventobject, sectionNumber, rowNumber) {
+        var self = this;
+
+        function SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_True() {
+            self.choiseCity.call(this, rowNumber);
+        }
+        function SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_False() {}
+        function SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_Callback(response) {
+            if (response === true) {
+                SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_True();
+            } else {
+                SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_False();
+            }
+        }
+        kony.ui.Alert({
+            "alertType": constants.ALERT_TYPE_CONFIRMATION,
+            "alertTitle": "Add City",
+            "yesLabel": "Yes",
+            "noLabel": "No",
+            "alertIcon": "plus_add_green.png",
+            "message": "Do you really want to add this city?",
+            "alertHandler": SHOW_ALERT__hb7da2d4b0414d04b74bdbaed1728ef6_Callback
+        }, {
+            "iconPosition": constants.ALERT_ICON_POSITION_LEFT
+        });
+    },
+    /** onTouchStart defined for segSearchCity **/
+    AS_Segment_ib5eb11a78b54e3c8ce355e4470b40bf: function AS_Segment_ib5eb11a78b54e3c8ce355e4470b40bf(eventobject, x, y) {
+        var self = this;
+    },
     /** onClick defined for btnGetCities **/
     AS_Button_a45c38c70fe044d2aa9efcacdb87d9de: function AS_Button_a45c38c70fe044d2aa9efcacdb87d9de(eventobject) {
         var self = this;
-        return self.getCity.call(this);
+        return self.showNewCity.call(this);
+    },
+    /** onClick defined for flxSearch **/
+    AS_FlexContainer_gef274a357c34e07adffe73812bace0e: function AS_FlexContainer_gef274a357c34e07adffe73812bace0e(eventobject) {
+        var self = this;
     }
 });
