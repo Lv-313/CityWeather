3f9f113834226e6a040b2095136445eb0652e2f8
Ivan Pykalo <ipykaloi@gmail.com>
2018-06-02 19:42:43 +0300

    create login servise

--- controllers/mobile/formLoginController/formLoginControllerActions.js
+++ controllers/mobile/formLoginController/formLoginControllerActions.js
@@ -2,4 +2,9 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for btnLogin **/
+    AS_Button_ga583fce0fb542c39e3a161bc1313bad: function AS_Button_ga583fce0fb542c39e3a161bc1313bad(eventobject) {
+        var self = this;
+        return self.login.call(this);
+    }
 });
